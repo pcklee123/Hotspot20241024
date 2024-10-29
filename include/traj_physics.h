20f37f4da8df699abff3ad9872aab1d0ed2cafaf
pcklee123 <99310789+pcklee123@users.noreply.github.com>
2024-10-29 09:32:32 +0800

    a

--- include/traj_physics.h
+++ include/traj_physics.h
@@ -7,7 +7,7 @@
                    // #define cylinder //do hot rod problem
 //#define quadrant   // do problem 1/4 sphere or cylinder
 #define Weibull
-constexpr float weibullb = 2; // b factor for weibull distribn. larger means closer to a shell. ~1 means filled more at the center.
+constexpr float weibullb = 1; // b factor for weibull distribn. larger means closer to a shell. ~1 means filled more at the center.
 #define Temp_e 7e6            // in Kelvin 1e7 ~1keV
 #define Temp_d 7e6            // in Kelvin
 constexpr int f1 = 1000;      // make bigger to make smaller time steps // 300 is min for sphere slight increase in KE
@@ -30,10 +30,10 @@ constexpr float r0_f[3] = {(float)n_space / 8.0, (float)n_space / 8.0, (float)n_
 constexpr float Bz0 = 0.00000;     // in T, static constant fields
 constexpr float Btheta0 = 0.00000; // in T, static constant fields
 constexpr float Ez0 = 0.0e0;       // in V/m
-constexpr float vz0 = 0.0f;
+constexpr float vz0 = 3.0e7f;
 constexpr float a0 = 4e-5;                          // typical dimensions of a cell in m This needs to be smaller than debye length otherwise energy is not conserved if a particle moves across a cell
 constexpr float a0_ff = 1.0 + 1.0 / (float)n_space; // rescale cell size, if particles rollover this cannot encrement more than 1 cell otherwise will have fake "waves"
-constexpr float target_part = 1e16;                 // 3.5e22 particles per m^3 per torr of ideal gas. 7e22 electrons for 1 torr of deuterium
+constexpr float target_part = 1e12;                 // 3.5e22 particles per m^3 per torr of ideal gas. 7e22 electrons for 1 torr of deuterium
 constexpr float v0_r = 0;                           // initial directed radial velocity outwards is positive
 
 // The maximum expected E and B fields. If fields go beyond this, the the time step, cell size etc will be wrong. Should adjust and recalculate.
@@ -46,8 +46,8 @@ constexpr float Emax0 = Ez0 + 1;                // 1e11V/m is approximately inte
 // Te 1e7,Td 1e7,B 0.1,E 1e8,nback 64, a0 0.1e-3,part 1e10,nspace 32 npartd *4 sphere, r1=1.8
 // a sphere 0.4 mm radius with 1e24*4/3*pi()*0.4^3 *1.6e-19C E on surface =2.4e12Vm-1 if all electrons have left.
 // r0=8*a0 Te 1e7,Td 1e7,B 100,E 1e10,nback 64, a0 1e-3,part 1e15,nspace 64 npartd *4 cylinder
-constexpr int ndatapoints = 50; // total number of time steps to print
-constexpr int nc1 = 25;          // f1 * 1;      // number of times to calculate E and B between printouts total number of electron time steps calculated = ndatapoints *nc1*md_me
+constexpr int ndatapoints = 10; // total number of time steps to print
+constexpr int nc1 = 1;          // f1 * 1;      // number of times to calculate E and B between printouts total number of electron time steps calculated = ndatapoints *nc1*md_me
 constexpr int md_me = 60;       // ratio of electron speed/deuteron speed at the same KE. Used to calculate electron motion more often than deuteron motion
 #define Hist_n 512
 // #define Hist_max Temp_e / 11600 * 60 // in eV Kelvin to eV is divide by 11600
